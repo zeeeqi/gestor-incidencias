97507bbed14c9b7326a7af9697420686c2d2c467
Ezequiel Arenilla <ezequielarenilla@gmail.com>
2022-02-15 12:41:42 +0100

    Migrations

---
+++ database/migrations/2022_02_15_104654_create_usuarios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('usuario')->unique();
+            $table->string('password');
+            $table->string('email')->unique();
+            $table->timestamp('ultimo_login');
+            $table->string('rol');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
---
+++ database/migrations/2022_02_15_104706_create_incidencias_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incidencias', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->text('contenido');
+            $table->foreignId('user_id_creado')->constrained('usuarios');
+            $table->foreignId('user_id_reparado')->constrained('usuarios');
+            $table->string('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incidencias');
+    }
+};
